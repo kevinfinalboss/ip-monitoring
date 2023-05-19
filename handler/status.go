ae5958710ac10983328d1eb44f9e6793408719ec
Kevin Gomes <kevinmg50@gmail.com>
2023-05-19 13:58:38 -0300

    feat: agora consulte qualquer URL ou IP

--- handler/status.go
+++ handler/status.go
@@ -2,27 +2,24 @@ package handler
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/kevinfinalboss/ip-monitoring/services"
 )
 
 func GetStatus(w http.ResponseWriter, r *http.Request) {
-	url := "https://github.com/"
-
-	statusCode, err := services.GetIPStatus(url)
-	if err != nil {
-		log.Printf("Erro ao obter o status da URL: %v", err)
-		http.Error(w, "Erro interno do servidor", http.StatusInternalServerError)
+	url := r.URL.Query().Get("url")
+	if url == "" {
+		http.Error(w, "Missing URL parameter", http.StatusBadRequest)
 		return
 	}
 
-	response := map[string]interface{}{
-		"url":    url,
-		"status": statusCode,
+	status, err := services.GetIPStatus(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(status)
 }
