508d7af243683a1f24984a32f6f775f0344cf07c
Kevin Gomes <kevinmg50@gmail.com>
2023-05-19 19:35:50 -0300

    fix: corrigido erro de parametro da url ser vazia

--- handler/status.go
+++ handler/status.go
@@ -7,14 +7,18 @@ import (
 	"github.com/kevinfinalboss/ip-monitoring/services"
 )
 
-func GetStatus(w http.ResponseWriter, r *http.Request) {
+type StatusHandler struct {
+	Services services.IPStatusGetter
+}
+
+func (h *StatusHandler) GetStatus(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
 	if url == "" {
 		http.Error(w, "Missing URL parameter", http.StatusBadRequest)
 		return
 	}
 
-	status, err := services.GetIPStatus(url)
+	status, err := h.Services.GetIPStatus(url)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
