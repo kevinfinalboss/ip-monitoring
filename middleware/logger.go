95c6508430cce984e0c50b5c6e9e11bed1663a65
Kevin Gomes <kevinmg50@gmail.com>
2023-05-19 13:27:44 -0300

    feat: adicionado logrus para rastreabilidade

---
+++ middleware/logger.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+func RequestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(startTime)
+		logrus.WithFields(logrus.Fields{
+			"method":     r.Method,
+			"requestURI": r.RequestURI,
+			"remoteAddr": r.RemoteAddr,
+			"duration":   duration,
+		}).Info("Processed a request")
+	})
+}
