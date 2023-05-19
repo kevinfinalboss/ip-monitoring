1e7826b5a6254ca757777b3ae878c53403244263
Kevin Gomes <kevinmg50@gmail.com>
2023-05-19 13:22:44 -0300

    fix: melhorado lógica para consultar URL

--- handler/status.go
+++ handler/status.go
@@ -2,12 +2,27 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/kevinfinalboss/ip-monitoring/services"
 )
 
 func GetStatus(w http.ResponseWriter, r *http.Request) {
+	url := "https://github.com/"
+
+	statusCode, err := services.GetIPStatus(url)
+	if err != nil {
+		log.Printf("Erro ao obter o status da URL: %v", err)
+		http.Error(w, "Erro interno do servidor", http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"url":    url,
+		"status": statusCode,
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
+	json.NewEncoder(w).Encode(response)
 }
